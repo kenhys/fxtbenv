ce1e177595c031b12e7eb41965c35a1a6f46be3c
Kentaro Hayashi <kenhys@gmail.com>
2018-01-18 19:00:40 +0900

    Add sample script to download archives

---
+++ examples/download_releases.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+#
+# Sample script to download firefox/thunderbird linux(x86-64) binary files
+#
+
+function download_product() {
+    product=$1
+    version=$2
+    BASE=https://ftp.mozilla.org
+    file="${product}-${version}.tar.bz2"
+    work="pub/${product}/releases/${version}/linux-x86_64/ja"
+    path="$work/$file"
+    mkdir -p $work
+    if [ ! -f $path ]; then
+	url="$BASE/$path"
+	echo "download: $url"
+	(cd $work && wget -q $url)
+    else
+	echo "skip download: $path"
+    fi
+}
+
+
+download_product thunderbird 45.0
+ESR45=`w3m -dump https://ftp.mozilla.org/pub/thunderbird/releases/ | cut -d' ' -f3 | \grep "45.*" | \grep -v b`
+for v in $ESR45; do
+    version=${v%/}
+    download_product thunderbird $version
+done
+download_product thunderbird 52.0
+ESR52=`w3m -dump https://ftp.mozilla.org/pub/thunderbird/releases/ | cut -d' ' -f3 | \grep "52.*" | \grep -v 52.0`
+for v in $ESR52; do
+    version=${v%/}
+    download_product thunderbird $version
+done
+
+download_product firefox 45.0esr
+ESR45=`w3m -dump https://ftp.mozilla.org/pub/firefox/releases/ | cut -d' ' -f3 | \grep "45.*esr" | \grep -v 45.0esr`
+for v in $ESR45; do
+    version=${v%/}
+    download_product firefox $version
+done
+download_product firefox 52.0esr
+ESR52=`w3m -dump https://ftp.mozilla.org/pub/firefox/releases/ | cut -d' ' -f3 | \grep "52.*esr" | \grep -v 52.0esr`
+for v in $ESR52; do
+    version=${v%/}
+    download_product firefox $version
+done
+
