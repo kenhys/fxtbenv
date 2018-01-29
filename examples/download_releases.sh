6a61680d8bb1e59b2b66504f2635ab5c5f16e48e
Kentaro Hayashi <kenhys@gmail.com>
2018-01-29 11:18:15 +0900

    Comment out

--- examples/download_releases.sh
+++ examples/download_releases.sh
@@ -24,28 +24,36 @@ function download_product() {
 }
 
 
-download_product thunderbird 45.0
-ESR45=`w3m -dump https://ftp.mozilla.org/pub/thunderbird/releases/ | cut -d' ' -f3 | \grep "45.*" | \grep -v b`
-for v in $ESR45; do
-    version=${v%/}
-    download_product thunderbird $version
-done
-download_product thunderbird 52.0
-ESR52=`w3m -dump https://ftp.mozilla.org/pub/thunderbird/releases/ | cut -d' ' -f3 | \grep "52.*" | \grep -v 52.0`
-for v in $ESR52; do
-    version=${v%/}
-    download_product thunderbird $version
-done
+# download_product thunderbird 45.0
+# ESR45=`w3m -dump https://ftp.mozilla.org/pub/thunderbird/releases/ | cut -d' ' -f3 | \grep "45.*" | \grep -v b`
+# for v in $ESR45; do
+#     version=${v%/}
+#     download_product thunderbird $version
+# done
+# download_product thunderbird 52.0
+# ESR52=`w3m -dump https://ftp.mozilla.org/pub/thunderbird/releases/ | cut -d' ' -f3 | \grep "52.*" | \grep -v 52.0`
+# for v in $ESR52; do
+#     version=${v%/}
+#     download_product thunderbird $version
+# done
 
-download_product firefox 45.0esr
-ESR45=`w3m -dump https://ftp.mozilla.org/pub/firefox/releases/ | cut -d' ' -f3 | \grep "45.*esr" | \grep -v 45.0esr`
-for v in $ESR45; do
-    version=${v%/}
-    download_product firefox $version
-done
-download_product firefox 52.0esr
-ESR52=`w3m -dump https://ftp.mozilla.org/pub/firefox/releases/ | cut -d' ' -f3 | \grep "52.*esr" | \grep -v 52.0esr`
-for v in $ESR52; do
+# download_product firefox 45.0esr
+# ESR45=`w3m -dump https://ftp.mozilla.org/pub/firefox/releases/ | cut -d' ' -f3 | \grep "45.*esr" | \grep -v 45.0esr`
+# for v in $ESR45; do
+#     version=${v%/}
+#     download_product firefox $version
+# done
+# download_product firefox 52.0esr
+# ESR52=`w3m -dump https://ftp.mozilla.org/pub/firefox/releases/ | cut -d' ' -f3 | \grep "52.*esr" | \grep -v 52.0esr`
+# for v in $ESR52; do
+#     version=${v%/}
+#     download_product firefox $version
+# done
+
+# download_product firefox 57
+VERSION=57
+LIST=`w3m -dump https://ftp.mozilla.org/pub/firefox/releases/ | cut -d' ' -f3 | \grep "$VERSION.*" | \grep -v esr | \grep -v 0b`
+for v in $LIST; do
     version=${v%/}
     download_product firefox $version
 done
