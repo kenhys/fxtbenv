b7facfa1672c2c936f7eabb0f35f4bf9c81b0107
Kentaro Hayashi <kenhys@gmail.com>
2018-02-01 20:25:44 +0900

    Remove comment

--- examples/download_releases.sh
+++ examples/download_releases.sh
@@ -24,32 +24,6 @@ function download_product() {
 }
 
 
-# download_product thunderbird 45.0
-# ESR45=`w3m -dump https://ftp.mozilla.org/pub/thunderbird/releases/ | cut -d' ' -f3 | \grep "45.*" | \grep -v b`
-# for v in $ESR45; do
-#     version=${v%/}
-#     download_product thunderbird $version
-# done
-# download_product thunderbird 52.0
-# ESR52=`w3m -dump https://ftp.mozilla.org/pub/thunderbird/releases/ | cut -d' ' -f3 | \grep "52.*" | \grep -v 52.0`
-# for v in $ESR52; do
-#     version=${v%/}
-#     download_product thunderbird $version
-# done
-
-# download_product firefox 45.0esr
-# ESR45=`w3m -dump https://ftp.mozilla.org/pub/firefox/releases/ | cut -d' ' -f3 | \grep "45.*esr" | \grep -v 45.0esr`
-# for v in $ESR45; do
-#     version=${v%/}
-#     download_product firefox $version
-# done
-# download_product firefox 52.0esr
-# ESR52=`w3m -dump https://ftp.mozilla.org/pub/firefox/releases/ | cut -d' ' -f3 | \grep "52.*esr" | \grep -v 52.0esr`
-# for v in $ESR52; do
-#     version=${v%/}
-#     download_product firefox $version
-# done
-
 # download_product firefox 57
 PRODUCT=$1
 VERSION=$2
