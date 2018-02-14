eeff6968ef9bd9c848defc0fdc090e8474c04ebd
Kentaro Hayashi <kenhys@gmail.com>
2018-02-14 14:06:58 +0900

    Check whether matched to fxtbenv

--- scripts/fxtbenv.zsh
+++ scripts/fxtbenv.zsh
@@ -6,16 +6,19 @@ function fxtbenv_chpwd() {
     RCFILE=$(pwd)/.fxtbenvrc
     if [ -f "$RCFILE" ]; then
 	line=`\grep "fxtbenv" $RCFILE`
-	command=`echo $line | cut -d' ' -f2`
-	profile=`echo $line | cut -d' ' -f3`
-	case $command in
-	    use)
-		eval $line
-		;;
-	    *)
-		:
-		;;
-	esac
+	if [ $? -eq 0 ]; then
+	    command=`echo $line | cut -d' ' -f2`
+	    profile=`echo $line | cut -d' ' -f3`
+	    case $command in
+		use)
+		    eval $line
+		    ;;
+		*)
+		    :
+		    ;;
+	    esac
+	else
+	fi
     fi
 }
 
