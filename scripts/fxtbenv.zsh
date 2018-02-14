a03559ad0e706224187a3c3030e7fdf1cc42b5c4
Kentaro Hayashi <kenhys@gmail.com>
2018-02-14 19:25:55 +0900

    Support multiple line in fxtbenvrc

--- scripts/fxtbenv.zsh
+++ scripts/fxtbenv.zsh
@@ -4,13 +4,14 @@ source $(dirname $0)/fxtbenv
 
 function fxtbenv_eval() {
     line=$1
-    command=`echo $line | cut -d' ' -f2`
-    profile=`echo $line | cut -d' ' -f3`
+    command=`echo "$line" | cut -d' ' -f2`
+    profile=`echo "$line" | cut -d' ' -f3`
     case $command in
 	use)
 	    eval $line
 	    ;;
 	*)
+	    echo "Failed to set $line"
 	    :
 	    ;;
     esac
@@ -19,19 +20,16 @@ function fxtbenv_eval() {
 function fxtbenv_chpwd() {
     RCFILE=$(pwd)/.fxtbenvrc
     if [ -f "$RCFILE" ]; then
-	line=`\grep "fxtbenv" $RCFILE`
-	if [ $? -eq 0 ]; then
-	    fxtbenv_eval $line
-	else
-	    line=`\grep "fxenv" $RCFILE`
-	    if [ $? -eq 0 ]; then
-		fxtbenv_eval $line
-	    fi
-	    line=`\grep "tbenv" $RCFILE`
-	    if [ $? -eq 0 ]; then
-		fxtbenv_eval $line
-	    fi
-	fi
+	cat $RCFILE | while read LINE || [ -n "${LINE}" ]; do
+	    case $LINE in
+		fxtbenv*|fxenv*|tbenv*)
+		    fxtbenv_eval $LINE
+		    ;;
+		*)
+		    echo "LINE: ${LINE}"
+		    ;;
+	    esac
+	done
     fi
 }
 
