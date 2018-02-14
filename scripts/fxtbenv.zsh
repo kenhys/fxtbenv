829c4f497e671c50fce55d85a09b01749e2d10db
Kentaro Hayashi <kenhys@gmail.com>
2018-02-14 14:11:48 +0900

    Support to parse current .fxtbenvrc

--- scripts/fxtbenv.zsh
+++ scripts/fxtbenv.zsh
@@ -2,22 +2,35 @@
 
 source $(dirname $0)/fxtbenv
 
+function fxtbenv_eval() {
+    line=$1
+    command=`echo $line | cut -d' ' -f2`
+    profile=`echo $line | cut -d' ' -f3`
+    case $command in
+	use)
+	    eval $line
+	    ;;
+	*)
+	    :
+	    ;;
+    esac
+}
+
 function fxtbenv_chpwd() {
     RCFILE=$(pwd)/.fxtbenvrc
     if [ -f "$RCFILE" ]; then
 	line=`\grep "fxtbenv" $RCFILE`
 	if [ $? -eq 0 ]; then
-	    command=`echo $line | cut -d' ' -f2`
-	    profile=`echo $line | cut -d' ' -f3`
-	    case $command in
-		use)
-		    eval $line
-		    ;;
-		*)
-		    :
-		    ;;
-	    esac
+	    fxtbenv_eval $line
 	else
+	    line=`\grep "fxenv" $RCFILE`
+	    if [ $? -eq 0 ]; then
+		fxtbenv_eval $line
+	    fi
+	    line=`\grep "tbenv" $RCFILE`
+	    if [ $? -eq 0 ]; then
+		fxtbenv_eval $line
+	    fi
 	fi
     fi
 }
